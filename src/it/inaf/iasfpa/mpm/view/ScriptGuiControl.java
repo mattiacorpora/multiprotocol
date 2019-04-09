cc0903a338aab9829ea33813b081783663cad693
mattia <mattia@mattia-SVP1322C5E>
2019-04-09 11:57:09 +0200

    in proceeding

---
+++ src/it/inaf/iasfpa/mpm/view/ScriptGuiControl.java
@@ -0,0 +1,16 @@
+package it.inaf.iasfpa.mpm.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ScriptGuiControl implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
